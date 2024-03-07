79290cdb8c4215935642c2763b849ecd5803e7ec
HayeongShin <judy.hayoung.shin@gmail.com>
2024-03-07 16:03:48 +0900

    chore : gitignore 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,23 @@ import TerserPlugin from 'terser-webpack-plugin';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  webpack: (config) => {
-    // 기존 대비 약 50% bundle file size 감소 효과
+  webpack: config => {
     config.optimization = {
       ...config.optimization,
-      minimize: isProduction, // minimizer 에 설정된 플러그인을 기반으로 번들을 최소화 시킴.
+      minimize: isProduction,
       minimizer: isProduction
         ? [
             new TerserPlugin({
-              parallel: true, // 병렬 작업
+              parallel: true,
               terserOptions: {
                 format: {
-                  comments: false, // 빌드 시, comment 제거 (주석 제거)
+                  comments: false,
                 },
                 compress: {
-                  drop_console: true, // 빌드 시, console.* 구문 코드 제거
+                  drop_console: true,
                 },
               },
-              extractComments: false, // 주석을 별도의 파일로 추출할 지 여부
+              extractComments: false,
             }),
           ]
         : [],
