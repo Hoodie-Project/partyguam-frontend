4e4961f6730f6c62290a0cd6061d4e2bf082b60c
HayeongShin <judy.hayoung.shin@gmail.com>
2024-03-24 03:53:16 +0900

    style : css flex -> grid

--- src/components/templates/join/Join.tsx
+++ src/components/templates/join/Join.tsx
@@ -73,18 +73,19 @@ export default function Join() {
 }
 
 const JoinContainer = styled.section`
-  padding-top: 84px;
+  padding: 5.25rem 21.25rem;
   width: 100%;
+
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 const JoinHeader = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 77.75rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;
+  width: 100%;
   height: 3.75rem;
 `;
 
@@ -96,7 +97,6 @@ const GoBackBtn = styled.button`
   font-weight: bold;
   font-size: 18px;
   color: var(--grey500);
-  margin-right: 30.375rem;
 `;
 
 const JoinForm = styled.section`
