4e7973bbb24ddf0141aebe56ae4d3f388a4287b0
HayeongShin <judy.hayoung.shin@gmail.com>
2025-02-20 15:14:42 +0900

    fix : BaseLayout 설정; context csr문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,32 @@
-'use client';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { Metadata } from 'next';
 
 import '@/styles/global.css';
-import { Modal } from '@/components/_molecules';
-import FormContextProvider from '@/contexts/FormContext/FormProvider';
-import ModalContextProvider from '@/contexts/ModalContext/ModalProvider';
 
-import Footer from './Footer';
-import Header from './Header';
+import BaseLayout from './BaseLayout';
 
-// export const metadata: Metadata = {
-//   title: 'PartyGuam',
-//   description: '파티 구해? partyguam!',
-// };
+export const metadata: Metadata = {
+  title: 'PartyGuam',
+  description: '파티 구해? partyguam!',
+  icons: {
+    icon: '/images/favicon.png',
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
   return (
     <html lang="kr">
       <body suppressHydrationWarning={true}>
-        <QueryClientProvider client={queryClient}>
-          <ModalContextProvider>
-            <FormContextProvider>
-              <Header />
-              <Modal />
-              <main>{children}</main>
-              <Footer />
-            </FormContextProvider>
-          </ModalContextProvider>
-        </QueryClientProvider>
+        <BaseLayout>
+          <main>{children}</main>
+        </BaseLayout>
       </body>
     </html>
   );
