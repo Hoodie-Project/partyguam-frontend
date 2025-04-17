83910ec8e07ff06022ad7e2bab540c23fbc2fa09
신하영 / Hayeong Shin <judy.hayoung.shin@gmail.com>
2025-04-17 12:41:42 +0900

    lint

--- src/components/features/notificationModal/index.tsx
+++ src/components/features/notificationModal/index.tsx
@@ -10,7 +10,7 @@ import { fetchDeleteNotification, fetchPatchReadNotification, type NotificationT
 import KebabMenu from '@/assets/icon/kebab-menu.svg';
 import { Chip, Txt } from '@/components/_atoms';
 import { DropdownV2 } from '@/components/_molecules/dropdown';
- 
+
 import { SFlexColumn, SFlexRow, SFlexRowCenter } from '@/styles/components';
 import { formatRelativeTime } from '@/utils/date';
 
@@ -52,7 +52,7 @@ export default function NotificationModal({ notificationData, onClose, filter, s
   const handleClickKebab = (id: number) => {
     setKebabSettingOpenId(id);
   };
-  
+
   return (
     <Container ref={containerRef}>
       <ModalContainer>
